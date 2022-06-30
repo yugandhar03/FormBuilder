753a63203890e1e5ec819a12da7333a8c2cbd673
ramasani asha <ramasaniasha@gmail.com>
2022-06-30 19:24:39 +0530

    client changes

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
+import SignUp from "./Components/Auth/SignUp";
+import Login from "./Components/Auth/Login";
+import ForgotPassword from "./Components/Auth/ForgotPassword";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element = {<Login/>}/>
+      <Route path ='/signup' element={<SignUp/>}/>
+      <Route path='/forgot' element = {<ForgotPassword/>}/>
+    </Routes>
+      {/* <SignUp /> */}
+      {/* <Login/> */}
+      {/* <ForgotPassword/> */}
+    </BrowserRouter>
+  );
+}
+
+export default App;
